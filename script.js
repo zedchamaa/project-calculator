cba91dbb657c6a1590c7a1ec32eb248ce8c3f4fd
Zed Chamaa <sales@nineprovince.com>
2022-06-22 11:04:47 +0200

    Create a function that subtracts two numbers

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ function add(num1, num2) {
   return total;
 }
 
+// Create a function that subtracts two numbers
 
+function subtract(num1, num2) {
+  let total = 0;
+  total = num1 - num2;
+  return total;
+}
 
