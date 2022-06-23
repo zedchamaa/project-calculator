08e8b73930677749a10eb1818c50602504b84a53
Zed Chamaa <sales@nineprovince.com>
2022-06-23 13:39:48 +0200

    Create the functions that populate the display screen when a number button is clicked

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 function add(num1, num2) {
   let total = 0;
   total = num1 + num2;
-  // return total;
-  console.log(total);
+  return total;
 }
 
 // Create a function that subtracts two numbers
@@ -12,8 +11,7 @@ function add(num1, num2) {
 function subtract(num1, num2) {
   let total = 0;
   total = num1 - num2;
-  // return total;
-  console.log(total);
+  return total;
 }
 
 // Create a function that multiplies two numbers
@@ -21,8 +19,7 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
   let total = 0;
   total = num1 * num2;
-  // return total;
-  console.log(total);
+  return total;
 }
 
 // Create a function that divides two numbers
@@ -30,8 +27,7 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   let total = 0;
   total = num1 / num2;
-  // return total;
-  console.log(total);
+  return total;
 }
 
 // Create a function that takes an operator and 2 numbers
@@ -51,6 +47,80 @@ function operate(num1, operator, num2) {
   }
 }
 
+// Store the display value
 
+let displayValue = 0;
 
-operate(100, "divide", 50);
+// Create the functions that populate the display when a number button is clicked
+
+const calculation = document.querySelector(".calculation");
+
+const buttonZero = document.getElementById("zero");
+buttonZero.addEventListener("click", populateDisplayZero);
+
+function populateDisplayZero() {
+  calculation.innerText += 0;
+}
+
+const buttonOne = document.getElementById("one");
+buttonOne.addEventListener("click", populateDisplayOne);
+
+function populateDisplayOne() {
+  calculation.innerText += 1;
+}
+
+const buttonTwo = document.getElementById("two");
+buttonTwo.addEventListener("click", populateDisplayTwo);
+
+function populateDisplayTwo() {
+  calculation.innerText += 2;
+}
+
+const buttonThree = document.getElementById("three");
+buttonThree.addEventListener("click", populateDisplayThree);
+
+function populateDisplayThree() {
+  calculation.innerText += 3;
+}
+
+const buttonFour = document.getElementById("four");
+buttonFour.addEventListener("click", populateDisplayFour);
+
+function populateDisplayFour() {
+  calculation.innerText += 4;
+}
+
+const buttonFive = document.getElementById("five");
+buttonFive.addEventListener("click", populateDisplayFive);
+
+function populateDisplayFive() {
+  calculation.innerText += 5;
+}
+
+const buttonSix = document.getElementById("six");
+buttonSix.addEventListener("click", populateDisplaySix);
+
+function populateDisplaySix() {
+  calculation.innerText += 6;
+}
+
+const buttonSeven = document.getElementById("seven");
+buttonSeven.addEventListener("click", populateDisplaySeven);
+
+function populateDisplaySeven() {
+  calculation.innerText += 7;
+}
+
+const buttonEight = document.getElementById("eight");
+buttonEight.addEventListener("click", populateDisplayEight);
+
+function populateDisplayEight() {
+  calculation.innerText += 8;
+}
+
+const buttonNine = document.getElementById("nine");
+buttonNine.addEventListener("click", populateDisplayNine);
+
+function populateDisplayNine() {
+  calculation.innerText += 9;
+}
